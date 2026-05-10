95f08d1ecc6a320a31327b3240c75eb29aeb7d4b
Mateusz Rachuta <mrachuta@thinkbox.pl>
2026-05-10 16:29:14 +0200

    Fix for path for parent module

--- kuper-cli.py
+++ kuper-cli.py
@@ -9,6 +9,12 @@ outside of the ``kuper`` package business logic.
 import argparse
 import datetime
 import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent
+SRC_DIR = ROOT_DIR / "src"
+if str(SRC_DIR) not in sys.path:
+    sys.path.insert(0, str(SRC_DIR))
 
 from kuper import (
     RUNNER,
